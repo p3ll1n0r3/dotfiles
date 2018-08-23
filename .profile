92a374125ab532892ca5140f6d11eedc27eb0325
John Snow <jsnow@darling.hellden.se>
2018-08-23 14:42:09 +0200

    many more files added

---
+++ .profile
@@ -0,0 +1,7 @@
+#
+# ~/.profile
+#
+
+[[ -f ~/.mash/functions ]] && . ~/.mash/functions
+[[ -f ~/.mash/aliases ]] && . ~/.mash/aliases
+[[ -f ~/.mash/enviroment ]] && . ~/.mash/enviroment
---
+++ .script/ffhistory.sh
@@ -0,0 +1,7 @@
+
+#/usr/bin/bash
+
+db=$(find "${HOME}/.mozilla/firefox/" -name "places.sqlite")
+query="select p.url from moz_historyvisits as h, moz_places as p where substr(h.visit_date, 0, 11) >= strftime('%s', date('now')) and p.id == h.place_id order by h.visit_date;"
+todays_urls=$(sqlite3 "${db}" "${query}")
+echo "${todays_urls}" > todays_urls
