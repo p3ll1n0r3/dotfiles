c1de9d15ee5f4ff9ee9fe8e26589720b52b4d05f
p3ll1n0r3 <42410269+p3ll1n0r3@users.noreply.github.com>
2020-12-27 12:29:38 +0100

    Create gmail.py

---
+++ .config/polybar/scripts/gmail.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import time
+import argparse
+import subprocess
+from pathlib import Path
+from googleapiclient import discovery, errors
+from google.oauth2.credentials import Credentials
+from httplib2 import ServerNotFoundError
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--label', default='INBOX')
+parser.add_argument('-p', '--prefix', default='\uf0e0')
+parser.add_argument('-c', '--color', default='#e06c75')
+parser.add_argument('-ns', '--nosound', action='store_true')
+args = parser.parse_args()
+
+DIR = Path(__file__).resolve().parent
+CREDENTIALS_PATH = Path(DIR, 'credentials.json')
+
+unread_prefix = '%{F' + args.color + '}' + args.prefix + ' %{F-}'
+#error_prefix = '%{F' + args.color + '}\uf06a %{F-}'
+count_was = 0
+
+creds = Credentials.from_authorized_user_file(CREDENTIALS_PATH)
+gmail = discovery.build('gmail', 'v1', credentials=creds)
+labels = gmail.users().labels().get(userId='me', id=args.label).execute()
+count = labels['messagesUnread']
+
+print (args.prefix + '  ' + str(count))
+
+#print_count(0, True)
+#
+
+#while True:
+#    try:
+#        if Path(CREDENTIALS_PATH).is_file():
+#            count_was = update_count(count_was)
+#            time.sleep(10)
+#        else:
+#            print(error_prefix + 'credentials not found', flush=True)
+#            time.sleep(2)
+#    except errors.HttpError as error:
+#        if error.resp.status == 404:
+#            print(error_prefix + f'"{args.label}" label not found', flush=True)
+#        else:
+#            print_count(count_was, True)
+#        time.sleep(5)
+#   # except (ServerNotFoundError, OSError):
+#        print_count(count_was, True)
+#        time.sleep(5)
